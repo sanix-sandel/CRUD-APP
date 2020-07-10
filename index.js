6f0e6280da0bd2cf883f3219f0bc93174542a062
sanix sandel <sanicksikani@gmail.com>
2020-07-11 02:08:26 +0300

    handling POST request with error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi=require('joi');
 const express=require('express')
 
 const app=express()
@@ -19,6 +20,21 @@ app.get('/api/courses', (req, res)=>{
 })
 
 app.post('/api/courses', (req, res)=>{
+
+    const schema={
+        name:Joi.string().min(3).required()
+    }
+
+    const result=Joi.validate(req.body, schema);
+
+  //  console.log(result);
+
+
+    if (result.error){
+        res.status(400).send(result.error.details[0].message)
+        return ;
+    }
+
     const course ={
         id:courses.length+1, 
         name:req.body.name
